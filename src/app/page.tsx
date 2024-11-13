230076d74ba197f6581b5c0ab6d84f256821c6c7
Tobias Halomoan <agyasta1808@gmail.com>
2024-11-13 11:11:20 +0700

    Fix : Fixed linting errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import { Button } from "@/components/ui/button";
 // Access environment variables (API in the future.)
-import { apiKey, apiUrl } from "@/lib/env";
+// import { apiKey, apiUrl } from "@/lib/env";
 
 export default function Home() {
-  console.log(apiKey);
   return (
     <>
       <Button className="mx-5 my-10 bg-pink-50 text-black">Hello world!</Button>
